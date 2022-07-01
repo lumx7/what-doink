aa025eed9eed5fb05201fc70eb0e47c7a4cdd75c
Lux <64970263+lumx7@users.noreply.github.com>
2022-07-01 11:12:20 -0400

    first commit

---
+++ src/initialize.cpp
@@ -0,0 +1,46 @@
+#include "main.h"
+#include "project/gui.hpp"
+#include <iostream>
+
+void auton_init();
+
+void initialize() {
+    puts("what the fuck is this shit");
+    fflush(stdout);
+    printf("Initializing...\n");
+    fflush(stdout);
+    
+
+    fprintf(stderr,"hello-err");
+    printf("run\n"); // reset graph
+
+    // Wait for PROS to initialize.
+    pros::delay(150);
+
+    /*
+    pros::lcd::initialize();
+    pros::lcd::set_text(1, "Goodbye PROS User.");
+
+    pros::lcd::register_btn1_cb(on_center_button);
+    */
+
+    robot::init(); // Initialise anything that needs to be.
+    auton_init(); // add all auton routines to selector
+
+    std::shared_ptr<GUI> gui = GUI::get();
+    gui->gui_build();
+    //  pros::lcd::initialize();
+    pros::delay(150);
+    fprintf(stderr, "Initialized.\n");
+}
+
+void disabled() {}
+
+void competition_initialize() {
+    // robot::chassis->inertial.reset();
+    std::shared_ptr<GUI> gui = GUI::get();
+    while (true) {
+        gui->update_inertial_status(robot::chassis->inertial);
+        pros::delay(20);
+    }
+}
